e952e43968926618407fba205156ee511fec2a0e
Delgermurun <delgermurun.p@nmma.co>
2012-05-25 21:06:08 +0800

    Part 5 done

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
+from .models import Choice
+
 
 class PollVoteForm(forms.Form):
     vote = forms.ChoiceField(widget=forms.RadioSelect())
 
-    def __init__(self, poll, initial):
-        forms.Form.__init__(self, initial)
+    def __init__(self, poll, **kwargs):
+        forms.Form.__init__(self, **kwargs)
         self.fields['vote'].choices = [(c.pk, c.choice) for c in poll.choice_set.all()]
+    
+    def add_vote(self):
+        choice = Choice.objects.get(pk=self.cleaned_data['vote'])
+        choice.votes += 1
+        choice.save()
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, FormView
+from django.core.urlresolvers import reverse
 
 from .models import Poll
 from .forms import PollVoteForm
@@ -15,13 +16,26 @@ class Home(Base, ListView):
 class Detail(Base, FormView):
     form_class = PollVoteForm
     template_name = 'polls/poll_detail.html'
+    object = None
+
+    def get_success_url(self):
+        return reverse('polls-detail', args=[self.object.pk])
+
+    def form_valid(self, form):
+        form.add_vote()
+        return FormView.form_valid(self, form)
+
+    def get_object(self):
+        if self.object is None:
+            self.object = Poll.objects.get(pk=self.kwargs['pk'])
+        return self.object
 
     def get_form_kwargs(self, **kwargs):
         kwargs = super(Detail, self).get_form_kwargs(**kwargs)
-        kwargs['poll'] = Poll.objects.get(pk=self.kwargs['pk'])
+        kwargs['poll'] = self.get_object()
         return kwargs
 
     def get_context_data(self, **kwargs):
         kwargs = super(Detail, self).get_context_data(**kwargs)
-        kwargs['poll'] = Poll.objects.get(pk=self.kwargs['pk'])
+        kwargs['poll'] = self.get_object()
         return kwargs
